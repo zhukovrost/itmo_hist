5caaea02bcfca1210b8f2086373ef8499905cd75
Zhukov Rostislav <rosf.zhukov@mail.ru>
2025-05-13 21:51:35 +0300

    photo update

--- models/location.py
+++ models/location.py
@@ -10,13 +10,17 @@ class Location:
         'photo'
     ]
 
-    def __init__(self, id, name, coords, description, history, photo=None):
+    def __init__(self, id, name, coords, description, history):
         self.id = id
         self.name = name
         self.coords = coords
         self.description = description
         self.history = history
-        self.photo = photo
+        self.photo = []
+
+    def add_photo(self, photo_link: str) -> None:
+        """Добавить фото в массив фоток локации."""
+        self.photo.append(photo_link)
 
     def __repr__(self):
         return f"<\"Точка {self.id}\": {self.name}>"
